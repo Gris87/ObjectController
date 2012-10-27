c8bdc3eac974292310b441f1712cf864bfb4e057
Gris87 <Gris87@yandex.ru>
2012-10-27 21:46:06 +0400

    BitEditor functions

--- src/property/widgets/biteditor.cpp
+++ src/property/widgets/biteditor.cpp
@@ -1,5 +1,11 @@
 #include "biteditor.h"
 
+#include <QPainter>
+#include <QScrollBar>
+#include <QKeyEvent>
+#include <QApplication>
+#include <QClipboard>
+
 #include <math.h>
 
 #define LINE_INTERVAL 2
@@ -8,4 +14,404 @@
 BitEditor::BitEditor(QWidget *parent) :
     QAbstractScrollArea(parent)
 {
+    for (int i=0; i<256; ++i)
+    {
+        char aChar=i;
+        mAsciiChars.append(QString::fromLatin1(&aChar, 1));
+    }
+
+    mAsciiChars[9] =QChar(26);
+    mAsciiChars[10]=QChar(8629);
+    mAsciiChars[13]=QChar(8601);
+    mAsciiChars[32]=QChar(183);
+
+    for (int i=128; i<=160; ++i)
+    {
+        mAsciiChars[i]=QChar(9734);
+    }
+
+
+
+    mMode=INSERT;
+    mReadOnly=false;
+    mCursorPosition=0;
+
+    mFont=QFont("Courier new", 1);     //Special action to calculate mCharWidth and mCharHeight at the next step
+    setFont(QFont("Courier new", 10));
+
+    mSelectionStart=0;
+    mSelectionEnd=0;
+    mSelectionInit=0;
+
+    mCursorVisible=true;
+    mCursorAtTheLeft=true;
+    connect(&mCursorTimer, SIGNAL(timeout()), this, SLOT(cursorBlicking()));
+    mCursorTimer.start(500);
+}
+
+void BitEditor::undo()
+{
+    mUndoStack.undo();
+    emit dataChanged();
+
+    setCursorPosition(mCursorPosition);
+    cursorMoved(false);
+
+    updateScrollBars();
+    viewport()->update();
+}
+
+void BitEditor::redo()
+{
+    mUndoStack.redo();
+    emit dataChanged();
+
+    setCursorPosition(mCursorPosition);
+    cursorMoved(false);
+
+    updateScrollBars();
+    viewport()->update();
+}
+
+void BitEditor::cursorBlicking()
+{
+    mCursorVisible=!mCursorVisible;
+    viewport()->update();
+}
+
+void BitEditor::scrollToCursor()
+{
+
+}
+
+// ------------------------------------------------------------------
+
+void BitEditor::updateScrollBars()
+{
+    mAddressWidth=0;
+    int aDataSize=mData.size();
+    int aCurSize=1;
+
+    while (aCurSize<aDataSize)
+    {
+        ++mAddressWidth;
+        aCurSize<<=4;
+    }
+
+    if (mAddressWidth==0)
+    {
+        ++mAddressWidth;
+    }
+
+    mLinesCount=floor(aDataSize/8.0f);
+
+    if (aDataSize % 8!=0)
+    {
+        ++mLinesCount;
+    }
+
+
+
+    int aTotalWidth=(mAddressWidth+13)*mCharWidth; // mAddressWidth + 1+8+1 + 1
+    int aTotalHeight=mLinesCount*mCharHeight;
+
+    if (mLinesCount>0)
+    {
+        aTotalHeight+=(mLinesCount-1)*LINE_INTERVAL;
+    }
+
+
+
+    QSize areaSize=viewport()->size();
+
+    horizontalScrollBar()->setPageStep(areaSize.width());
+    verticalScrollBar()->setPageStep(areaSize.height());
+
+    horizontalScrollBar()->setRange(0, aTotalWidth  - areaSize.width()  + 1);
+    verticalScrollBar()->setRange(  0, aTotalHeight - areaSize.height() + 1);
+}
+
+void BitEditor::resetCursorTimer()
+{
+    mCursorVisible=true;
+
+    mCursorTimer.stop();
+    mCursorTimer.start(500);
+
+    viewport()->update();
+}
+
+void BitEditor::resetSelection()
+{
+}
+
+void BitEditor::updateSelection()
+{
+}
+
+void BitEditor::cursorMoved(bool aKeepSelection)
+{
+    if (aKeepSelection)
+    {
+        updateSelection();
+    }
+    else
+    {
+        resetSelection();
+    }
+
+    resetCursorTimer();
+    scrollToCursor();
+}
+
+void BitEditor::resizeEvent(QResizeEvent *event)
+{
+    QAbstractScrollArea::resizeEvent(event);
+    updateScrollBars();
+}
+
+void BitEditor::paintEvent(QPaintEvent */*event*/)
+{
+
+}
+
+void BitEditor::keyPressEvent(QKeyEvent *event)
+{
+
+}
+
+void BitEditor::mousePressEvent(QMouseEvent *event)
+{
+    /*
+    mLeftButtonPressed=(event->button()==Qt::LeftButton);
+
+    if (mLeftButtonPressed)
+    {
+        bool aShift=event->modifiers() & Qt::ShiftModifier;
+        int aPosition=charAt(event->pos(), &mCursorAtTheLeft);
+
+        if (aShift)
+        {
+            if ((aPosition>>1)>=mSelectionInit)
+            {
+                aPosition+=2;
+            }
+            else
+            {
+                mOneMoreSelection=true;
+            }
+        }
+
+        setCursorPosition(aPosition);
+        cursorMoved(aShift);
+    }
+    */
+
+    QAbstractScrollArea::mousePressEvent(event);
+}
+
+void BitEditor::mouseMoveEvent(QMouseEvent *event)
+{
+    /*
+    if (mLeftButtonPressed)
+    {
+        int aPosition=charAt(event->pos(), &mCursorAtTheLeft);
+
+        if ((aPosition>>1)>=mSelectionInit)
+        {
+            aPosition+=2;
+        }
+        else
+        {
+            mOneMoreSelection=true;
+        }
+
+        setCursorPosition(aPosition);
+        cursorMoved(true);
+    }
+    */
+
+    QAbstractScrollArea::mouseMoveEvent(event);
+}
+
+void BitEditor::mouseReleaseEvent(QMouseEvent *event)
+{
+    /*
+    mLeftButtonPressed=false;
+    */
+
+    QAbstractScrollArea::mouseReleaseEvent(event);
+}
+
+void BitEditor::wheelEvent(QWheelEvent *event)
+{
+    if (event->delta()>=0)
+    {
+        verticalScrollBar()->setValue(verticalScrollBar()->value()-10*(mCharHeight+LINE_INTERVAL));
+    }
+    else
+    {
+        verticalScrollBar()->setValue(verticalScrollBar()->value()+10*(mCharHeight+LINE_INTERVAL));
+    }
+}
+
+// ------------------------------------------------------------------
+
+QBitArray BitEditor::data() const
+{
+    return mData;
+}
+
+void BitEditor::setData(QBitArray const &aData)
+{
+    if (mData!=aData)
+    {
+        mData=aData;
+        setCursorPosition(mCursorPosition);
+        mUndoStack.clear();
+
+        updateScrollBars();
+        viewport()->update();
+
+        emit dataChanged();
+    }
+}
+
+BitEditor::Mode BitEditor::mode() const
+{
+    return mMode;
+}
+
+void BitEditor::setMode(const Mode &aMode)
+{
+    if (mMode!=aMode)
+    {
+        mMode=aMode;
+
+        resetCursorTimer();
+
+        emit modeChanged(mMode);
+    }
+}
+
+bool BitEditor::isReadOnly() const
+{
+    return mReadOnly;
+}
+
+void BitEditor::setReadOnly(const bool &aReadOnly)
+{
+    mReadOnly=aReadOnly;
+}
+
+int BitEditor::position() const
+{
+    return mCursorPosition>>1;
+}
+
+void BitEditor::setPosition(int aPosition)
+{
+    if ((mCursorPosition>>1)!=aPosition)
+    {
+        setCursorPosition(aPosition<<1);
+    }
+}
+
+qint64 BitEditor::cursorPosition() const
+{
+    return mCursorPosition;
+}
+
+void BitEditor::setCursorPosition(qint64 aCursorPos)
+{
+    if (aCursorPos<0)
+    {
+        aCursorPos=0;
+    }
+    else
+    if (aCursorPos>mData.size()<<1)
+    {
+        aCursorPos=mData.size()<<1;
+    }
+
+    if (mCursorPosition!=aCursorPos)
+    {
+        bool aSamePos=((mCursorPosition>>1)==(aCursorPos>>1));
+
+        mCursorPosition=aCursorPos;
+        viewport()->update();
+
+        if (!aSamePos)
+        {
+            emit positionChanged(mCursorPosition>>1);
+        }
+    }
+}
+
+QFont BitEditor::font() const
+{
+    return mFont;
+}
+
+void BitEditor::setFont(const QFont &aFont)
+{
+    if (mFont!=aFont)
+    {
+        mFont=aFont;
+
+        QFontMetrics aFontMetrics(mFont);
+
+        int aCharWidth1=aFontMetrics.width('0');
+        int aCharWidth2=aFontMetrics.width('1');
+
+        if (aCharWidth1>aCharWidth2)
+        {
+            mCharWidth=aCharWidth1;
+        }
+        else
+        {
+            mCharWidth=aCharWidth2;
+        }
+
+        mCharWidth+=CHAR_INTERVAL;
+        mCharHeight=aFontMetrics.height()+CHAR_INTERVAL;
+
+        updateScrollBars();
+        viewport()->update();
+    }
+}
+
+int BitEditor::charWidth()
+{
+    return mCharWidth;
+}
+
+int BitEditor::charHeight()
+{
+    return mCharHeight;
+}
+
+quint8 BitEditor::addressWidth()
+{
+    return mAddressWidth;
+}
+
+int BitEditor::linesCount()
+{
+    return mLinesCount;
+}
+
+int BitEditor::selectionStart()
+{
+    return mSelectionStart;
+}
+
+int BitEditor::selectionEnd()
+{
+    return mSelectionEnd;
+}
+
+bool BitEditor::isCursorAtTheLeft()
+{
+    return mCursorAtTheLeft;
 }
--- src/property/widgets/biteditor.h
+++ src/property/widgets/biteditor.h
@@ -3,11 +3,31 @@
 
 #include <QAbstractScrollArea>
 
+#include <QUndoCommand>
+#include <QTimer>
+#include <QBitArray>
+
 class BitEditor : public QAbstractScrollArea
 {
     Q_OBJECT
 
 public:
+    Q_PROPERTY(QBitArray    Data                     READ data                     WRITE setData)
+    Q_PROPERTY(Mode         Mode                     READ mode                     WRITE setMode)
+    Q_PROPERTY(bool         ReadOnly                 READ isReadOnly               WRITE setReadOnly)
+    Q_PROPERTY(qint64       Position                 READ position                 WRITE setPosition)
+    Q_PROPERTY(int          CursorPosition           READ cursorPosition           WRITE setCursorPosition)
+    Q_PROPERTY(QFont        Font                     READ font                     WRITE setFont)
+
+    Q_PROPERTY(int      charWidth      READ charWidth)
+    Q_PROPERTY(int      charHeight     READ charHeight)
+    Q_PROPERTY(quint8   addressWidth   READ addressWidth)
+    Q_PROPERTY(int      linesCount     READ linesCount)
+
+    Q_PROPERTY(int    SelectionStart    READ selectionStart)
+    Q_PROPERTY(int    SelectionEnd      READ selectionEnd)
+    Q_PROPERTY(bool   CursorAtTheLeft   READ isCursorAtTheLeft)
+
     Q_ENUMS(Mode)
 
     enum Mode
@@ -19,6 +39,87 @@ public:
 
 
     BitEditor(QWidget *parent = 0);
+
+
+
+    void scrollToCursor();
+
+    // ------------------------------------------------------------------
+
+    QBitArray data() const;
+    void setData(QBitArray const &aData);
+
+    Mode mode() const;
+    void setMode(const Mode &aMode);
+
+    bool isReadOnly() const;
+    void setReadOnly(const bool &aReadOnly);
+
+    int position() const;
+    void setPosition(int aPosition);
+
+    qint64 cursorPosition() const;
+    void setCursorPosition(qint64 aCursorPos);
+
+    QFont font() const;
+    void setFont(const QFont &aFont);
+
+    int    charWidth();
+    int    charHeight();
+    quint8 addressWidth();
+    int    linesCount();
+
+    int  selectionStart();
+    int  selectionEnd();
+    bool isCursorAtTheLeft();
+
+protected:
+    QBitArray  mData;
+    Mode       mMode;
+    bool       mReadOnly;
+    qint64     mCursorPosition;
+    QFont      mFont;
+
+    QString    mAsciiChars;
+    int        mCharWidth;
+    int        mCharHeight;
+    quint8     mAddressWidth;
+    int        mLinesCount;
+
+    int        mSelectionStart;
+    int        mSelectionEnd;
+    int        mSelectionInit;
+    bool       mCursorVisible;
+    bool       mCursorAtTheLeft;
+    QTimer     mCursorTimer;
+
+    QUndoStack mUndoStack;
+
+    void updateScrollBars();
+    void resetCursorTimer();
+    void resetSelection();
+    void updateSelection();
+    void cursorMoved(bool aKeepSelection);
+    void resizeEvent(QResizeEvent *event);
+    void paintEvent(QPaintEvent *event);
+    void keyPressEvent(QKeyEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+    void wheelEvent(QWheelEvent *event);
+
+public slots:
+    void undo();
+    void redo();
+
+protected slots:
+    void cursorBlicking();
+
+signals:
+    void dataChanged();
+    void selectionChanged(int aStart, int aEnd);
+    void modeChanged(Mode aMode);
+    void positionChanged(int aPosition);
 };
 
 #endif // BITEDITOR_H
