0b9e617e0a7c8bfac36ddd906e2a8c16451edf24
unknown <maxim.shvecov@ebechkalo-notebook.LGE.NET>
2012-12-17 06:52:24 +0400

    TextLengthEditDialog

--- src/property/dialogs/textlengtheditdialog.cpp
+++ src/property/dialogs/textlengtheditdialog.cpp
@@ -1,9 +1,35 @@
 #include "textlengtheditdialog.h"
 #include "ui_textlengtheditdialog.h"
 
+TextLengthEditDialog::TextLengthEditDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::TextLengthEditDialog)
+{
+    init(QTextLength(), 0);
+}
+
+TextLengthEditDialog::TextLengthEditDialog(QTextLength aTextLength, QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::TextLengthEditDialog)
+{
+    init(aTextLength, 0);
+}
+
+TextLengthEditDialog::TextLengthEditDialog(const PropertyAttributes *aAttributes, QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::TextLengthEditDialog)
+{
+    init(QTextLength(), aAttributes);
+}
+
 TextLengthEditDialog::TextLengthEditDialog(QTextLength aTextLength, const PropertyAttributes *aAttributes, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::TextLengthEditDialog)
+{
+    init(aTextLength, aAttributes);
+}
+
+void TextLengthEditDialog::init(QTextLength aTextLength, const PropertyAttributes *aAttributes)
 {
     ui->setupUi(this);
 
--- src/property/dialogs/textlengtheditdialog.h
+++ src/property/dialogs/textlengtheditdialog.h
@@ -16,6 +16,9 @@ class TextLengthEditDialog : public QDialog
     Q_OBJECT
 
 public:
+    explicit TextLengthEditDialog(QWidget *parent = 0);
+    explicit TextLengthEditDialog(QTextLength aTextLength, QWidget *parent = 0);
+    explicit TextLengthEditDialog(const PropertyAttributes *aAttributes, QWidget *parent = 0);
     explicit TextLengthEditDialog(QTextLength aTextLength, const PropertyAttributes *aAttributes, QWidget *parent = 0);
     ~TextLengthEditDialog();
 
@@ -23,6 +26,8 @@ public:
 
 private:
     Ui::TextLengthEditDialog *ui;
+
+    void init(QTextLength aTextLength, const PropertyAttributes *aAttributes);
 
 private slots:
     void on_okButton_clicked();
